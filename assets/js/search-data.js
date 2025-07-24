d7865a8c5b5767bd2153320ede32777e35a9600b
rpatel264 <rpatel264@users.noreply.github.com>
2025-07-24 03:01:23 +0000

    deploy: 30229cb26823048902a3fb9e0a95292f4e2b91d8

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -30,6 +30,13 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/portfolio/projects/";
           },
+        },{id: "nav-creative-endeavors",
+          title: "Creative Endeavors",
+          description: "",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/portfolio/creative/";
+          },
         },{id: "nav-repositories",
           title: "repositories",
           description: "Edit the `_data/repositories.yml` and change the `github_users` and `github_repos` lists to include your own GitHub profile and repositories.",
