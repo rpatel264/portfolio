2669a0c3ec6cbf0970c1e5665d336a3975e1ae6d
rpatel264 <rpatel264@users.noreply.github.com>
2025-07-24 03:16:19 +0000

    deploy: 6116637aa153d0d8521bdf55dbc407c03e259dc1

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -37,19 +37,19 @@ ninja.data = [{
               handler: () => {
                 window.location.href = "/portfolio/creative/photography/";
               },
-            },{id: "nav-repositories",
-          title: "repositories",
-          description: "Edit the `_data/repositories.yml` and change the `github_users` and `github_repos` lists to include your own GitHub profile and repositories.",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/portfolio/repositories/";
-          },
-        },{id: "nav-cv",
+            },{id: "nav-cv",
           title: "cv",
           description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/portfolio/cv/";
+          },
+        },{id: "nav-repositories",
+          title: "repositories",
+          description: "Edit the `_data/repositories.yml` and change the `github_users` and `github_repos` lists to include your own GitHub profile and repositories.",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/portfolio/repositories/";
           },
         },{id: "post-a-post-with-plotly-js",
         
